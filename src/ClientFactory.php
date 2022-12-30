05125fea050d452ac3c0194d5e09f44ee060ff1a
yanggeng <yanggeng@126.com>
2022-12-30 14:16:21 +0800

    first commit

---
+++ src/ClientFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\ConfigConsul;
+
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Guzzle\ClientFactory as GuzzleClientFactory;
+use Hyperf\Utils\Network;
+use Psr\Container\ContainerInterface;
+
+class ClientFactory
+{
+    public function __invoke(ContainerInterface $container)
+    {
+        $config = $container->get(ConfigInterface::class);
+        /** @var \Hyperf\ConfigApollo\Option $option */
+        $option = make(Option::class);
+        $option->setServer($config->get('config_center.drivers.consul.server', 'http://127.0.0.1:8080'))
+            ->setAppid($config->get('config_center.drivers.consul.appid', ''))
+            ->setCluster($config->get('config_center.drivers.consul.cluster', ''))
+            ->setClientIp($config->get('config_center.drivers.consul.client_ip', Network::ip()))
+            ->setPullTimeout($config->get('config_center.drivers.consul.pull_timeout', 10))
+            ->setIntervalTimeout($config->get('config_center.drivers.consul.interval_timeout', 60))
+            ->setSecret($config->get('config_center.drivers.consul.secret', ''));
+        $httpClientFactory = function (array $options = []) use ($container) {
+            return $container->get(GuzzleClientFactory::class)->create($options);
+        };
+        return make(Client::class, compact('option', 'httpClientFactory'));
+    }
+}
